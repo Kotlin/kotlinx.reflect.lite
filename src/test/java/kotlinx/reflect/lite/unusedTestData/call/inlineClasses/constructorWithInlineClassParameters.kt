3c0365db43dfbeb9d626e0ae953c7ce05487cc57
mvicsokolova <maria.sokolova@jetbrains.com>
2022-07-29 13:19:38 +0200

    move more unsupported tests to unusedTestData

--- src/test/java/kotlinx/reflect/lite/unusedTestData/call/inlineClasses/constructorWithInlineClassParameters.kt
+++ src/test/java/kotlinx/reflect/lite/unusedTestData/call/inlineClasses/constructorWithInlineClassParameters.kt
@@ -11,18 +11,18 @@ class Outer(val z1: Z) {
 }
 
 inline class InlineOuter(val z1: Z) {
-    @Suppress("INNER_CLASS_INSIDE_INLINE_CLASS")
-    inner class Inner(val z2: Z) {
-        val test = "$z1 $z2"
-    }
+    //@Suppress("INNER_CLASS_INSIDE_INLINE_CLASS")
+//    inner class Inner(val z2: Z) {
+//        val test = "$z1 $z2"
+//    }
 }
 
 fun box(): String {
     assertEquals(Z(1), ::Outer.call(Z(1)).z1)
     assertEquals("Z(x=1) Z(x=2)", Outer::Inner.call(Outer(Z(1)), Z(2)).test)
     assertEquals("Z(x=1) Z(x=3)", Outer(Z(1))::Inner.call(Z(3)).test)
-    assertEquals("Z(x=1) Z(x=2)", InlineOuter::Inner.call(InlineOuter(Z(1)), Z(2)).test)
-    assertEquals("Z(x=1) Z(x=3)", InlineOuter(Z(1))::Inner.call(Z(3)).test)
+//    assertEquals("Z(x=1) Z(x=2)", InlineOuter::Inner.call(InlineOuter(Z(1)), Z(2)).test)
+//    assertEquals("Z(x=1) Z(x=3)", InlineOuter(Z(1))::Inner.call(Z(3)).test)
 
     return "OK"
 }
